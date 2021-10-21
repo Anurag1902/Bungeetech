ad3277977fd965909326c3af406b800709e856a1
Sirius097 <aasingh2vin@gmail.com>
2021-10-21 12:46:29 +0530

    done with q1

--- output/q1.py
+++ output/q1.py
@@ -4,15 +4,11 @@ import numpy as np
 # reading the data
 display = pd.read_csv('main.csv')
 
-#converting the given 
-display.Year = pd.to_datetime(display.Year, format = '%Y')
-display.head()
-#
-display.set_index('Year', drop=True, inplace=True)
-display.head()
+#group by year with the range of 10 years and summing the coloumns except Population
+data = display.groupby(np.floor(display['Year']/10) * 10)[['Violent','Property','Murder','Forcible_Rape','Robbery','Aggravated_assault','Burglary','Larceny_Theft','Vehicle_Theft']].sum();
 
-data = display.resample('10AS').sum()
-data['Population'] = display['Population'].resample('10AS').max()
-print (data)
+#adding population coloumn with the max value 
+data['Population'] = display['Population'].groupby(np.floor(display['Year']/10) * 10).max()
 
+print(data)
 #Code done by Anurag
